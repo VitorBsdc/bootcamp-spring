9f6d99775cdd10b2741b69f813fe50cecb7add7d
Vitor Benedito <jose.benedito.costa05@gmail.com>
2022-01-31 16:58:14 -0300

    Modificações pequenas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,21 +10,21 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+        <h1 className="text-primary py-3">Dashboard Vendas</h1>
 
          <div className="row px-3">
            <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de Sucesso(%)</h5>
+            <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
             <BarChart />
            </div>
            <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Todas vendas</h5>
+            <h5 className="text-center text-secondary">Vendas Totais</h5>
             <DonutChart />
            </div>
          </div>
 
          <div className="py-3">
-           <h2 className="text-primary">Todas Vendas</h2>
+           <h2 className="text-primary">Vendas Totais</h2>
          </div>
 
         <DataTable />
