1938c95d14dda24cd24e880c8486e201580b6006
Vitor Benedito <vitor.bsdc@gmail.com>
2022-02-05 18:50:51 -0300

    BarChart Integration

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -4,32 +4,32 @@ import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
-const DashBoard = () => {
-  return (
-    <>
-      <NavBar />
-      <div className="container">
-        <h1 className="text-primary py-3">Dashboard Vendas</h1>
+const Dashboard = () => {
+    return (
+        <>
+            <NavBar />
+            <div className="container">
+                <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
-        <div className="row px-3">
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
-            <BarChart />
-          </div>
-          <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Vendas Totais</h5>
-            <DonutChart />
-          </div>
-        </div>
+                <div className="row px-3">
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Taxa de Sucesso(%)</h5>
+                        <BarChart />
+                    </div>
+                    <div className="col-sm-6">
+                        <h5 className="text-center text-secondary">Todas vendas</h5>
+                        <DonutChart />
+                    </div>
+                </div>
 
-        <div className="py-3">
-          <h2 className="text-primary">Vendas Totais</h2>
-        </div>
+                <div className="py-3">
+                    <h2 className="text-primary">Todas Vendas</h2>
+                </div>
 
-        <DataTable />
-      </div>
-      <Footer />
-    </>
-  );
-};
-export default DashBoard;
+                <DataTable />
+            </div>
+            <Footer />
+        </>
+    );
+}
+export default Dashboard;
